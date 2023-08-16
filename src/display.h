82ae1f7935983c7b34538dab26ab3c531e9e7020
kingking <kingking1223@gmail.com>
2023-08-16 14:07:46 +0800

    Create window

--- src/display.h
+++ src/display.h
@@ -1,16 +1,23 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <SDL2/SDL.h>
 #include <string>
 
 class Display {
     public:
         Display(int width, int height, const std::string& title);
+        void Clear(float r, float g, float b, float a);
+        void Update();
+        bool IsClosed();
         virtual ~Display();
     protected:
     private:
         Display(const Display& other) {}
-        Display& operator=(const Display& other) {} 
+        void operator=(const Display& other) {} 
+        SDL_Window* m_window;
+        SDL_GLContext m_glcontext;
+        bool isClosed;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <GL/glew.h>
 #include "display.h"
 
 int main() 
 {
     Display display(800, 600, "test3");
+
+    while (!display.IsClosed()) {
+        display.Clear(0.0f, 0.25f, 0.6f, 1.0f);
+        display.Update();
+    }
     return 0;
 }
