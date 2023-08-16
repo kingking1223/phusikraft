144e0a2efa70cbe87dd41ce6ddc8182b249d1aae
kingking <kingking1223@gmail.com>
2023-08-16 15:24:28 +0800

    Create basic shaders for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "shader.h"
 
 int main() 
 {
     Display display(800, 600, "test3");
+
+    Shader shader("./res/testShader");
 
     while (!display.IsClosed()) {
         display.Clear(0.0f, 0.25f, 0.6f, 1.0f);
